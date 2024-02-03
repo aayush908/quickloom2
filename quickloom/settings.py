38dcdfc402755c5d52482ab3b826cad8b3292cce
Aayush kapil <aayushkapil100@gmail.com>
2024-02-03 14:25:47 +0530

    dss

--- quickloom/settings.py
+++ quickloom/settings.py
@@ -127,9 +127,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = [BASE_DIR/"static"]
-STATICFILES_DIRS =[ os.path.join(BASE_DIR, "staticfiles_build"),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URLS ='/media/'
+MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
